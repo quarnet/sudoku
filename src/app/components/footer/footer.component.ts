2e5ee3440b5e37aa4df334395616da3aea4d6fce
quarnet <patelaxay47@gmail.com>
2021-09-04 11:06:39 +0100

    Add footer with icons

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  links = [
+    {
+      title: 'instagram',
+      url: 'https://www.instagram.com/itsaxaypatel/',
+      icon: 'bi-instagram',
+    },
+    {
+      title: 'github',
+      url: 'https://github.com/quarnet',
+      icon: 'bi-github',
+    },
+    {
+      title: 'google',
+      url: 'mailto:patelaxay47@gmail.com',
+      icon: 'bi-google',
+    },
+    {
+      title: 'linkedin',
+      url: 'https://www.linkedin.com/in/-akshay-patel-/',
+      icon: 'bi-linkedin',
+    },
+  ];
+  ngOnInit(): void {}
 }
